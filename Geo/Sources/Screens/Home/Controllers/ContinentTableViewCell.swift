85d325b7f95c5d04deed8524df6bd1154dcc70cc
Hechio <stevehechio@gmail.com>
2021-10-06 13:59:19 +0300

    first commit

---
+++ Geo/Sources/Screens/Home/Controllers/ContinentTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ContinentTableViewCell.swift
+//  Geo
+//
+//  Created by Steve Hechio on 10/6/21.
+//
+
+import UIKit
+
+class ContinentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
